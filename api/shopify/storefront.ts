720d112b2c6fb2fa6c2b0c7d25752acc33b91da6
opulmkt-cmd <opulmkt@gmail.com>
2026-04-14 19:10:59 +0530

    Create storefront.ts

---
+++ api/shopify/storefront.ts
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      `https://${process.env.VITE_SHOPIFY_STORE_DOMAIN}/api/2024-01/graphql.json`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Storefront-Access-Token":
+            process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN!,
+        },
+        body: JSON.stringify(req.body.payload || req.body),
+      }
+    );
+
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+}
